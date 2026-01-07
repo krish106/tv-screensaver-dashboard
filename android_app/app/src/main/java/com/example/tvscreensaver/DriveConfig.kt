ca1ebcd2ae53a12c4d274b437d3716fd0a19a041
krish106 <krishp5777@gmail.com>
2026-01-07 14:08:55 +0530

    Remove source code - keep only documentation and APK releases

--- android_app/app/src/main/java/com/example/tvscreensaver/DriveConfig.kt
+++
@@ -1,6 +0,0 @@
-package com.example.tvscreensaver
-
-object DriveConfig {
-    const val WEB_CLIENT_ID = "950906764084-6g5cdj66k0qq5ssorrh8rbkj3b6n0ehq.apps.googleusercontent.com"
-    const val SCOPES = "https://www.googleapis.com/auth/drive.readonly"
-}
--- esp32_firmware_arduino/wifi_secrets.h
+++
@@ -1,7 +0,0 @@
-#ifndef WIFI_SECRETS_H
-#define WIFI_SECRETS_H
-
-const char *ssid = "Hi";
-const char *password = "Hello123";
-
-#endif
